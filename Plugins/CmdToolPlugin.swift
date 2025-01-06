efdd3a7b028033223193cc193b624d11a58ff621
Jozef Lipovsky <j.lipovsky@me.com>
2025-01-05 18:19:09 -0800

    Add mode logs

--- Plugins/CmdToolPlugin.swift
+++ Plugins/CmdToolPlugin.swift
@@ -15,6 +15,15 @@ struct CmdToolPlugin: CommandPlugin {
         try process.run()
         process.waitUntilExit()
 
+        let gracefulExit = process.terminationReason == .exit && process.terminationStatus == 0
+        if !gracefulExit {
+            let reason = process.terminationReason.rawValue
+            let status = process.terminationStatus
+            throw "\(String(describing: type(of: self))) failed - reason: \(reason) - status: \(status)"
+        }
+
         print("\(String(describing: type(of: self))) finished performCommand")
     }
 }
+
+extension String: @retroactive Error {}
