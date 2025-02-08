823c8c239b4375147c1c4da9d38d9bbd394f318b
Jozef Lipovsky <j.lipovsky@me.com>
2025-02-07 20:14:35 -0800

    Update commands, add reading handler and termination handler

--- Plugins/CmdToolPlugin.swift
+++ Plugins/CmdToolPlugin.swift
@@ -4,25 +4,48 @@ import Foundation
 @main
 struct CmdToolPlugin: CommandPlugin {
     func performCommand(context: PluginContext, arguments: [String]) async throws {
+        let log = String(describing: type(of: self))
+
         let tool = try context.tool(named: "CmdToolExecutable")
 
         let process = Process()
         process.executableURL = tool.url
         process.arguments = arguments
 
-        print("\(String(describing: type(of: self))) performCommand arguments: \(arguments.joined(separator: " "))")
+        let outputPipe = Pipe()
+        process.standardOutput = outputPipe
+        process.standardError = outputPipe
+
+        print("\(log) performCommand arguments: \(arguments.joined(separator: " "))")
+
+        outputPipe.fileHandleForReading.readabilityHandler = { [log] pipeHandle in
+            guard let data = try? pipeHandle.readToEnd() else {
+                return
+            }
+
+            print("\(log) outputPipe fileHandleForReading:\n\(String(data: data, encoding: .utf8)!)")
+        }
 
         try process.run()
+
+        process.terminationHandler = { _ in
+            do {
+                try outputPipe.fileHandleForReading.close()
+            } catch {
+                print("\(log) process terminationHandler - \(error.localizedDescription)")
+            }
+        }
+
         process.waitUntilExit()
 
         let gracefulExit = process.terminationReason == .exit && process.terminationStatus == 0
         if !gracefulExit {
             let reason = process.terminationReason.rawValue
             let status = process.terminationStatus
-            throw "\(String(describing: type(of: self))) failed - reason: \(reason) - status: \(status)"
+            throw "\(log) failed - reason: \(reason) - status: \(status)"
         }
 
-        print("\(String(describing: type(of: self))) finished performCommand")
+        print("\(log) finished performCommand")
     }
 }
 
--- Sources/Commands/ParsableCommand+Extensions.swift
+++ Sources/Commands/ParsableCommand+Extensions.swift
@@ -3,15 +3,37 @@ import Foundation
 
 extension ParsableCommand {
     func execute(arguments: [String]) throws {
+        let log = String(describing: type(of: self))
+
         let process = Process()
         process.executableURL = URL(fileURLWithPath: "/usr/bin/swift")
         process.arguments = arguments
 
-        print("\(String(describing: type(of: self))) executing arguments: \(arguments.joined(separator: " "))")
+        let outputPipe = Pipe()
+        process.standardOutput = outputPipe
+        process.standardError = outputPipe
+
+        print("\(log) executing arguments: \(arguments.joined(separator: " "))")
+
+        outputPipe.fileHandleForReading.readabilityHandler = { [log] pipeHandle in
+            guard let data = try? pipeHandle.readToEnd() else {
+                return
+            }
+
+            print("\(log) outputPipe fileHandleForReading:\n\(String(data: data, encoding: .utf8)!)")
+        }
 
         try process.run()
-        process.waitUntilExit()
 
-        print("\(String(describing: type(of: self))) finished executing")
+        process.terminationHandler = { _ in
+            do {
+                try outputPipe.fileHandleForReading.close()
+            } catch {
+                print("\(log) process terminationHandler - \(error.localizedDescription)")
+            }
+        }
+
+        process.waitUntilExit()
+        print("\(log) finished executing")
     }
 }
